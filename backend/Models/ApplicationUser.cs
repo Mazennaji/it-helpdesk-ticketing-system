cdb49831fb5bdc8b318afa67e0c62140724bcb51
Mazennaji <mazen.naji.1@gmail.com>
2026-07-16 21:54:05 +0300

    Add ApplicationUser model and .gitignore file; update AssemblyInfo and cache

---
+++ backend/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Backend.Models
+{
+    public class ApplicationUser : IdentityUser<int>
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
