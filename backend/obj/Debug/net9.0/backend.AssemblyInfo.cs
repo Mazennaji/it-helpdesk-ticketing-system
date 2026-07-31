7ce0504a2b982e424ada054297f32ceef534c753
Mazennaji <mazen.naji.1@gmail.com>
2026-07-31 13:18:18 +0300

    feat: update Dashboard component to fetch and display ticket data with improved animations

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3273847bd115266e331636a33bf001545c911878")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+455dab49c70bbd49d298dba42336484fdb18c5a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
