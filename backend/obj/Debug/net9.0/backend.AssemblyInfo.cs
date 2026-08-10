3200cae722a2958c8f50d7dceeedeac138d3fed4
Mazennaji <mazen.naji.1@gmail.com>
2026-08-10 13:23:27 +0300

    fix: update AssemblyInformationalVersion and AssemblyInfoInputs.cache

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89e791c2f3196bd81c993e35fd746030961510ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46fa1baf6046ed236548cbb493419c24e7d0e4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
